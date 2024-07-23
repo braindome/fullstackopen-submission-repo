1e313de0d192c991c3e99c1c9ab215f4034a82d7
Antonio Saracino <antonio.saracino83@gmail.com>
2024-07-23 19:03:01 +0200

    test

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -16,7 +16,7 @@ const Total = (exercises1, exercises2, exercises3) => {
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
+  const part1 = "Fundamentals of React -";
   const exercises1 = 10;
   const part2 = "Using props to pass data";
   const exercises2 = 7;
